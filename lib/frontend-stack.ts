fe758e01b47b6c3cbed0021de65f250439deae16
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-03-16 12:08:56 +0900

    Website用バケット作成

--- lib/frontend-stack.ts
+++ lib/frontend-stack.ts
@@ -8,10 +8,11 @@ import {
 	aws_iam,
 	aws_s3,
 } from "aws-cdk-lib";
-import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
-import * as route53 from "aws-cdk-lib/aws-route53";
-import * as targets from "aws-cdk-lib/aws-route53-targets";
 import type { Construct } from "constructs";
+
+// import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+// import * as route53 from "aws-cdk-lib/aws-route53";
+// import * as targets from "aws-cdk-lib/aws-route53-targets";
 
 /**
  * CloudFront + S3でホスティング用のStackを作成する
@@ -50,24 +51,24 @@ export class FrontendStack extends Stack {
 		// S3バケットポリシーにステートメントを追加する
 		websiteBucket.addToResourcePolicy(websiteBucketPolicyStatement);
 
-		// 利用するホストゾーンをドメイン名で取得
-		// ホストゾーンIDを取得
-		const hostedZoneId = route53.HostedZone.fromLookup(this, "HostedZoneId", {
-			domainName: "timetable-hideskick.net",
-		});
+		// // 利用するホストゾーンをドメイン名で取得
+		// // ホストゾーンIDを取得
+		// const hostedZoneId = route53.HostedZone.fromLookup(this, "HostedZoneId", {
+		// 	domainName: "timetable-hideskick.net",
+		// });
 
-		// 証明書を取得
-		const certificate = Certificate.fromCertificateArn(
-			this,
-			"Certificate",
-			"arn:aws:acm:us-east-1:851725614224:certificate/5b4f1664-f268-4e91-9461-31cccc26f0ca",
-		);
+		// // 証明書を取得
+		// const certificate = Certificate.fromCertificateArn(
+		// 	this,
+		// 	"Certificate",
+		// 	"arn:aws:acm:us-east-1:851725614224:certificate/5b4f1664-f268-4e91-9461-31cccc26f0ca",
+		// );
 
 		// CloudFrontディストリビューションを作成する
 		const distribution = new aws_cloudfront.Distribution(this, "Distribution", {
-			domainNames: ["timetable-hideskick.net"],
-			certificate,
-			comment: "timetable-hideskick.net-distribution",
+			// domainNames: ["timetable-hideskick.net"],
+			// certificate,
+			comment: "mem-vision-frontend-distribution",
 			defaultRootObject: "index.html",
 			defaultBehavior: {
 				allowedMethods: aws_cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
@@ -82,13 +83,13 @@ export class FrontendStack extends Stack {
 			priceClass: aws_cloudfront.PriceClass.PRICE_CLASS_100,
 		});
 
-		// Route53レコード設定
-		new route53.ARecord(this, "ARecord", {
-			zone: hostedZoneId,
-			target: route53.RecordTarget.fromAlias(
-				new targets.CloudFrontTarget(distribution),
-			),
-			recordName: "timetable-hideskick.net",
-		});
+		// // Route53レコード設定
+		// new route53.ARecord(this, "ARecord", {
+		// 	zone: hostedZoneId,
+		// 	target: route53.RecordTarget.fromAlias(
+		// 		new targets.CloudFrontTarget(distribution),
+		// 	),
+		// 	recordName: "timetable-hideskick.net",
+		// });
 	}
 }
