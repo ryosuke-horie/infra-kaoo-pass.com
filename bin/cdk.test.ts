587231666abbfce4f9823af849944986d578bb89
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-03-22 00:08:14 +0900

    cdk-nagを追記

--- bin/cdk.test.ts
+++ bin/cdk.test.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib";
+import { Aspects } from "aws-cdk-lib";
+import { AwsSolutionsChecks } from "cdk-nag";
+import "source-map-support/register";
 // import { CdkTestStack } from '../lib/cdk_test-stack';
 import { FrontendStack } from "../lib/frontend-stack";
 import { GithubCiCdStack } from "../lib/github-cicd-stack";
-import { AwsSolutionsChecks } from 'cdk-nag'
-import { Aspects } from 'aws-cdk-lib';
 
 const app = new cdk.App();
 // cdk-nag AwsSolutions Packを追加し、追加のログを有効化
-Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }))
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
 const env = { region: "ap-northeast-1", account: "905418074681" };
 
 new FrontendStack(app, "FrontendStack", { env });
