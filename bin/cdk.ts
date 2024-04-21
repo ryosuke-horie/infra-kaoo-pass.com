14681aaddd26a4a616dcecda6972680f4f34c7e4
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-04-21 23:43:37 +0900

    ネイキッドドメインでの管理のフロントスタックをaccountに変更

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { AdminFrontendStack } from "../lib/admin-frontend-stack";
-import { FrontendStack } from "../lib/frontend-stack";
+import { AccountFrontendStack } from "../lib/account-frontend-stack";
 import { GithubCiCdStack } from "../lib/github-cicd-stack";
 import { MypageFrontendStack } from "../lib/mypage-frontend-stack";
 
@@ -10,7 +10,7 @@ const app = new cdk.App();
 const env = { region: "ap-northeast-1", account: "905418074681" };
 
 // フロントエンドアプリケーションスタック
-new FrontendStack(app, "FrontendStack", { env });
+new AccountFrontendStack(app, "FrontendStack", { env });
 
 // 管理サイトフロントスタック
 new AdminFrontendStack(app, "AdminFrontendStack", { env });
